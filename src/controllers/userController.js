f67967c4fe28c5a6fd929b651c556f3413f16f25
Ian Jairo <ijtg.ian@gmail.com>
2024-12-02 01:13:13 -0300

    Feature: Padronização de respostas da API

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,7 @@
 const User = require('../models/user');
 const { validationResult } = require('express-validator');
 const { createUserValidation, alterUserValidation } = require('./../utils/validations/userValidations');
-
+const  createResponse = require('./../utils/helpers/responseHelper');
 exports.createUser = [
   createUserValidation,
   async (req, res) => {
@@ -9,10 +9,23 @@ exports.createUser = [
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json(
+        createResponse({
+          status: 'Erro',
+          message: 'Erro de validação.',
+          errors: errors.array(),
+        })
+      );
     }
     const user = await User.create({ nome, sobrenome, email, senha });
-    res.status(201).json(user);
+
+    res.status(201).json(
+      createResponse({
+        status: 'Sucesso',
+        message: 'Usuário cadastrado com sucesso!',
+        data: {"id": user["id_usuario"]}
+      })
+    );
   },
 ];
 
@@ -33,14 +46,26 @@ exports.alterUser = [
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json(
+        createResponse({
+          status: 'Erro',
+          message: 'Erro de validação.',
+          errors: errors.array(),
+        })
+      );
     }
-
+    
     const user = await User.update(
       { nome, sobrenome, email, senha },
       { where: { id_usuario: userID } }
     );
-    res.status(201).json(user);
+    res.status(201).json(
+      createResponse({
+        status: 'Sucesso',
+        message: 'Usuário alterado com sucesso!',
+        data: {"id": userID}
+      })
+    );
   }
 ]
 
