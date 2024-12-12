9000bb828b17bcafdb6f5fa6f0e5d588b9424271
João Gabriel S. Dantas <gabrielsd.10000@gmail.com>
2024-12-12 12:39:21 -0300

    Fix: indexModel
    Não foi enviado no commit anterior (Fix: Grupo apos...)

--- src/models/indexModel.js
+++ src/models/indexModel.js
@@ -1,30 +1,40 @@
+const MemberGroup = require('../models/groupMember');
 const Group = require('./group');
 const User = require('./user');
 const Address = require('./address');
 const Room = require('./room');
 const RoomMember = require('./roomMember');
 
-User.belongsToMany(Group, {through: 'membros_grupos'});
-Group.belongsToMany(User, {through: 'membros_grupos'});
+User.belongsToMany(Group, { 
+    through: MemberGroup, 
+    foreignKey: 'id_usuario', 
+    otherKey: 'id_grupo',
+  });
+  
+  Group.belongsToMany(User, { 
+    through: MemberGroup, 
+    foreignKey: 'id_grupo', 
+    otherKey: 'id_usuario',
+  });
+  
+// User.associate({ Address });
+// Address.associate({ User });
 
-User.associate({ Address });
-Address.associate({ User });
+// // Associações para Room
+// Room.associate = (models) => {
+//     Room.belongsTo(models.User, {
+//         foreignKey: 'id_usuario',
+//         as: 'creator',
+//     });
 
-// Associações para Room
-Room.associate = (models) => {
-    Room.belongsTo(models.User, {
-        foreignKey: 'id_usuario',
-        as: 'creator',
-    });
+//     Room.belongsTo(models.Group, {
+//         foreignKey: 'id_grupo',
+//         as: 'group',
+//     });
+// };
 
-    Room.belongsTo(models.Group, {
-        foreignKey: 'id_grupo',
-        as: 'group',
-    });
-};
-
-Room.belongsToMany(User, { through: RoomMember, foreignKey: 'id_room', as: 'members' });
-User.belongsToMany(Room, { through: RoomMember, foreignKey: 'id_user', as: 'rooms' });
+// Room.belongsToMany(User, { through: RoomMember, foreignKey: 'id_room', as: 'members' });
+// User.belongsToMany(Room, { through: RoomMember, foreignKey: 'id_user', as: 'rooms' });
 
 
 module.exports = {
